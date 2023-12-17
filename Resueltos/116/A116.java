29ffee08cc4474985835cfd12e465e935d98a4ac
PabloTaber <pablo.tabernerovidal@gmail.com>
2023-12-17 14:18:03 +0100

    Actualizado 116

--- Resueltos/116/A116.java
+++ Resueltos/116/A116.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Hola {
+public class A116 {
     public static void main(String[] args) throws Exception {
         
         Scanner sc = new Scanner(System.in);
