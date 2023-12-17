346386bfa6778fefb26741cf0643a6c47a4f8359
PabloTaber <137113370+PabloTaber@users.noreply.github.com>
2023-12-17 18:37:10 +0100

    Update and rename En Proceso/Identidad/src/Identidad.java to Resueltos/151/A151.java

--- Resueltos/151/A151.java
+++ Resueltos/151/A151.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Identidad {
+public class A151 {
     public static void main(String[] args) throws Exception {
         
         Scanner sc = new Scanner(System.in);
