68d24d27579968bc92c5bdcced2dd7fec726429c
PabloTaber <pablo.tabernerovidal@gmail.com>
2023-12-17 18:58:45 +0100

    create A155.java

---
+++ Resueltos/155/A155.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class A155 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+
+		int ancho = entrada.nextInt();
+		int alto = entrada.nextInt();
+		
+        while (! (ancho < 0 ||alto < 0)) {
+			System.out.println(ancho * 2 + alto * 2);
+			ancho = entrada.nextInt();
+			alto = entrada.nextInt();
+		}
+	}
+}
